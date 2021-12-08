e8981ffed0e73c9d6fcbcc189de3d287ba5615fa
b3n3m <47480089+b3n3m@users.noreply.github.com>
2021-12-08 19:52:03 +0100

    typo

--- liveRename.sh
+++ liveRename.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-echo "START renmaing: $(date +%Y-%m-%d_%H:%M:%S)" > rename.log
+echo "START renaming: $(date +%Y-%m-%d_%H:%M:%S)" > rename.log
+
 for f in *.HEIC; do 
-name=`exiftool -s -s -s -P -'DateTimeOriginal' -d %Y%m%d_%H%M%S $f`
+	name=`exiftool -s -s -s -P -'DateTimeOriginal' -d %Y%m%d_%H%M%S $f`
 
 # if files already in place
-i=1;while [[ -f "$name.HEIC" ]]; do
-	name="${name}_${i}"
-done
+	i=1; while [[ -f "$name.HEIC" ]]; do
+		name="${name}_${i}"
+	done
 
 # rename
-mv ${f} ${name}.HEIC
-echo "rename $f to ${name}.HEIC" >> rename.log
-if [[ -f ${f%.*}.MOV ]];then
-	mv ${f%.*}.MOV ${name}.MOV
-	echo "rename: ${f%.*}.MOV to ${name}.MOV" >> rename.log
-else
-	echo "${f%.*}.MOV - Not found" >> rename.log
-fi
-
+	mv ${f} ${name}.HEIC
+	echo "rename $f to ${name}.HEIC" >> rename.log
+	if [[ -f ${f%.*}.MOV ]];then
+		mv ${f%.*}.MOV ${name}.MOV
+		echo "rename: ${f%.*}.MOV to ${name}.MOV" >> rename.log
+	else
+		echo "${f%.*}.MOV - Not found" >> rename.log
+	fi
 done
+
+echo "END renaming: $(date +%Y-%m-%d_%H:%M:%S)" >> rename.log
